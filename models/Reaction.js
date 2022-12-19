9aaec46cb7a16a67fef46546968c3272cec26229
mvricole <marian.borja@gmail.com>
2022-12-18 21:18:28 -0800

    format reaction model and standard index.js

---
+++ models/Reaction.js
@@ -0,0 +1,36 @@
+const { Schema, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+// Sets up the formatting of the data on the Schema. 
+// Requires the reaction to have actual valid text input, require valid username 
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 150
+    },
+    username: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      // Get method formats the time stamp on query.
+      get: timestamp => dateFormat(timestamp)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+    },
+    id: false
+  }
+);
+
+module.exports = reactionSchema;
---
+++ models/index.js
@@ -0,0 +1,5 @@
+// Declares user and thoughts to be requirements for the application. 
+const User = require('./User');
+const Thought = require('./Thought');
+
+module.exports = { User, Thought };
